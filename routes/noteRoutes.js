f60a7103f446ccf01e3e055e33a33ee47ae29f08
Oghenetejiri Enaibe <enaibs99@gmail.com>
2026-06-24 16:11:52 +0100

    updated routes

---
+++ routes/noteRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const noteController = require('../controllers/noteController');
+
+// Root Route
+router.get('/', noteController.getWelcomeMessage);
+
+// Note CRUD Routes
+router.post('/notes', noteController.createNote);
+router.get('/notes', noteController.getAllNotes);
+router.get('/notes/:id', noteController.getNoteById);
+router.put('/notes/:id', noteController.updateNote);
+router.delete('/notes/:id', noteController.deleteNote);
+
+module.exports = router;
